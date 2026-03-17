45744ab8cb88773a5d1db1e6be9f9e671cb8c3cb
Kalle Lindqvist <kallelin@gmail.com>
2026-03-17 16:15:37 +0100

    register entities in db context

--- LMS.Infractructure/Data/ApplicationDbContext.cs
+++ LMS.Infractructure/Data/ApplicationDbContext.cs
@@ -7,6 +7,11 @@ namespace LMS.Infractructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
