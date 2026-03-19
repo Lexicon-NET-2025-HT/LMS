7bdd2e3db64f9feb960d07c7385424ff0a836c1b
Kalle Lindqvist <kallelin@gmail.com>
2026-03-19 09:33:54 +0100

    seed activities

--- LMS.API/Services/DataSeedHostingService.cs
+++ LMS.API/Services/DataSeedHostingService.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Domain.Models.Enums;
 using LMS.Infractructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -50,6 +51,9 @@ public class DataSeedHostingService : IHostedService
 
             var modules = FakeModules(2, 7, courses);
             await AddModulesToDb(modules);
+
+            var activities = FakeActivities(modules);
+            await AddActivitiesToDb(activities);
 
             logger.LogInformation("Seed complete");
         }
@@ -211,6 +215,114 @@ public class DataSeedHostingService : IHostedService
             if (!exists)
             {
                 await dbContext.Modules.AddAsync(module);
+            }
+        }
+
+        await dbContext.SaveChangesAsync();
+    }
+    private IEnumerable<Domain.Models.Entities.Activity> FakeActivities(IEnumerable<Module> modules)
+    {
+        var activities = new List<Domain.Models.Entities.Activity>();
+
+        var textFaker = new Faker("sv");
+
+        foreach (var module in modules)
+        {
+            var compareTime = module.StartDate;
+
+            while (compareTime < module.EndDate)
+            {
+                var type = textFaker.PickRandom<ActivityType>();
+                var (activityStart, activityEnd) = FakeActivityTimes(type, compareTime);
+                compareTime = activityEnd;
+
+                activities.Add(new Domain.Models.Entities.Activity
+                {
+                    ModuleId = module.Id,
+                    Name = FakeActivityName(type, textFaker),
+                    Type = type,
+                    Description = textFaker.Lorem.Paragraph(random.Next(1, 3)),
+                    StartTime = activityStart,
+                    EndTime = activityEnd
+                });
+            }
+        }
+
+        return activities;
+    }
+
+    private (DateTime?, DateTime) FakeActivityTimes(ActivityType type, DateTime afterTime)
+    {
+        var startTime = EnsureWeekday(afterTime.AddDays(random.Next(1, 3)));
+        return type switch
+        {
+            ActivityType.Lecture => (WithHour(9, startTime), WithHour(12, startTime)),
+            ActivityType.Exercise => (WithHour(10, startTime), WithHour(15, startTime)),
+            ActivityType.Assignment => (null, EnsureWeekday(afterTime.AddDays(random.Next(1, 5)))),
+            ActivityType.ELearning => (null, EnsureWeekday(afterTime.AddDays(random.Next(1, 5)))),
+            _ => (WithHour(9, startTime), WithHour(17, startTime))
+        };
+    }
+
+    private string FakeActivityName(ActivityType type, Faker textFaker)
+    {
+        return type switch
+        {
+            ActivityType.Lecture => textFaker.PickRandom(new[]
+            {
+                "Introduktion till ämnet",
+                "Genomgång av grunder",
+                "Fördjupning i koncept",
+                "Teori och principer"
+            }),
+
+            ActivityType.Exercise => textFaker.PickRandom(new[]
+            {
+                "Praktisk övning",
+                "Kodövning",
+                "Workshop",
+                "Parprogrammering"
+            }),
+
+            ActivityType.Assignment => textFaker.PickRandom(new[]
+            {
+                "Inlämningsuppgift 1",
+                "Projektuppgift",
+                "Hemuppgift",
+                "Case study"
+            }),
+
+            ActivityType.ELearning => textFaker.PickRandom(new[]
+            {
+                "Onlinekurs",
+                "Videogenomgång",
+                "Självstudier",
+                "Interaktiv modul"
+            }),
+
+            _ => textFaker.PickRandom(new[]
+            {
+                "Seminarium",
+                "Diskussion",
+                "Övrig aktivitet"
+            })
+        };
+    }
+
+    private async Task AddActivitiesToDb(IEnumerable<Domain.Models.Entities.Activity> activities)
+    {
+        if (dbContext is null)
+        {
+            throw new InvalidOperationException("DbContext is not initialized");
+        }
+        foreach (var activity in activities)
+        {
+            bool exists = await dbContext.Modules
+                .AnyAsync(c => c.Name == activity.Name);
+
+            if (!exists)
+            {
+                await dbContext.Activities.AddAsync(activity);
             }
         }
 
