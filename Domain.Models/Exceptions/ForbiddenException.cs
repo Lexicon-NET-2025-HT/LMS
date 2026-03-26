fec06521c87c7e91fe292f7f32eef1cf989373a3
Kalle Lindqvist <kallelin@gmail.com>
2026-03-26 16:38:03 +0100

    update exception title

--- Domain.Models/Exceptions/ForbiddenException.cs
+++ Domain.Models/Exceptions/ForbiddenException.cs
@@ -2,7 +2,7 @@ namespace Domain.Models.Exceptions;
 
 public class ForbiddenException : DomainException
 {
-    public ForbiddenException(string message, string title = "Bad Request")
+    public ForbiddenException(string message, string title = "Forbidden")
         : base(message, title, 403)
     {
     }
